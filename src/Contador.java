30809ca44331bbd6b51af46f268466aeb4bc5895
gsilva279 <josegilvanilson2020@gmail.com>
2025-04-04 16:31:26 -0300

    Meu primeiro commit com Bash

---
+++ src/Contador.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+
+		try { 
+			contar(parametroUm, parametroDois);
+		} catch(ParametrosInvalidosException e){
+			System.out.println("Erro: " + e.getMessage());
+		}  
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois){
+			throw new ParametrosInvalidosException("O primeiro parâmetro deve ser menor ou igual ao do segundo parâmetro.");
+		}
+
+		int contagem = parametroDois - parametroUm;
+		for(int i=0; i <= contagem; i++){
+				System.out.println("Imprimindo o número " + i);
+			}
+	}
+
+	static class ParametrosInvalidosException extends Exception {
+		public ParametrosInvalidosException(String mensagem){
+			//construtor para a mensagem
+			super(mensagem);
+		}
+	}
+}
